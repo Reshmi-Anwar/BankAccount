cf23796cd6fd05d91cf4532f31ff367d46abe47a
Reshmi-Anwar <ranwar10@stuy.edu>
2020-10-28 13:17:02 -0400

    added accessing methods

--- BankAccount.java
+++ BankAccount.java
@@ -4,8 +4,20 @@ public class BankAccount {
   private String password;
   public BankAccount (int accountID, String password){
     balance = 0;
+    this.password = password;
+    this.accountID = accountID;
   }
   public String toString(){
     return String.valueOf(accountID) + "\t" + String.valueOf(balance);
   }
+  public double getBalance(){
+    return balance;
+  }
+  public int getAccountID(){
+    return accountID;
+  }
+  public void setPassword(String newPassword){
+    password = newPassword;
+  }
+
 }
